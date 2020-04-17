0447866da94464552a95f16b095e5e638a1e2415
chooeeh <chchgml10@gmail.com>
2020-04-17 16:33:52 +0900

    2020/04/17 북마크 구현

--- app/src/main/java/com/example/prography_assignment_2/CItiesViewHolder.kt
+++ app/src/main/java/com/example/prography_assignment_2/CItiesViewHolder.kt
@@ -2,6 +2,7 @@ package com.example.prography_assignment_2
 
 
 import android.view.View
+import android.widget.CheckedTextView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -9,10 +10,16 @@ class CItiesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val tvNo = itemView.findViewById<TextView>(R.id.tv_no)
     val tvCity = itemView.findViewById<TextView>(R.id.tv_city)
     val tvUrl = itemView.findViewById<TextView>(R.id.tv_url)
+    val ctvStar = itemView.findViewById<CheckedTextView>(R.id.ctv_star)
 
     fun bind(data: CitiesInfoData){
         tvNo.text = data.no.toString()
         tvCity.text = data.city
         tvUrl.text = "url : " + data.url
+
+        ctvStar.setOnClickListener {
+            ctvStar.toggle()
+        }
     }
+    
 }
